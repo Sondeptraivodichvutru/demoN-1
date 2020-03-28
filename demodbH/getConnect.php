e041191f85e8531a1b9b98e1b2cb2848ba93e5af
Sondeptraivodichvutru <62633356+Sondeptraivodichvutru@users.noreply.github.com>
2020-03-28 14:15:28 +0700

    c

---
+++ demodbH/getConnect.php
@@ -0,0 +1,14 @@
+<?php
+$host="ec2-52-207-93-32.compute-1.amazonaws.com";
+$username="cfpqcmieuguxzc";
+$password="be1ff296c1e77f1c98793a8f224816674566f363be7ef2ba394d98aa8e9bb64a";
+$database="d425mt4bhmok93";
+$port="5432";
+$conn=mysqli_connect($host,$username,$port,$password,$database);
+mysqli_query($conn, $database);
+if (mysqli_connect_error())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+echo'hic';
+}
+?>
