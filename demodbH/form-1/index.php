2976a781c27e1356a2706125856e37726baadce3
Sondeptraivodichvutru <62633356+Sondeptraivodichvutru@users.noreply.github.com>
2020-04-01 12:06:59 +0700

    mm

--- demodbH/form-1/index.php
+++ demodbH/form-1/index.php
@@ -9,14 +9,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $sql = "SELECT * FROM tblaccount";// WHERE _name = '$myusername' and _pass = '$mypassword'";
     $result = pg_query($conn, $sql);
-    $count= mysqli_num_rows($result);
+    //$count= pg_fetch_row($result);
     
-    if($count==0){      
+    if(pg_fetch_row($result)){      
             echo ('Khá chắc kèo là mi sai tài khoản hoặc mật khẩu ');
-             echo $count;
+
     } else {
             echo ('đăng nhập thành công');
-             echo $count;
+
          header("location: ../wellcome.php");      
     }
     /*if ($count > 0) {
