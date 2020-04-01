01b3f5f6ddeb64cca5cb1c5b33b6332ff119affd
Sondeptraivodichvutru <62633356+Sondeptraivodichvutru@users.noreply.github.com>
2020-04-01 12:16:47 +0700

    âc

--- demodbH/form-1/index.php
+++ demodbH/form-1/index.php
@@ -13,7 +13,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     if (pg_fetch_row($result)){      
           echo 'Đăng nhập được rồi'; 
-          header("../wellcome.php");
+          header('Location:../wellcome.php');
     }else{
         echo 'Đăng nhập hem đc';
     }
