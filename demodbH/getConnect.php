7d1ffe374adc16ba89a49ec4607a8787230da32e
Sondeptraivodichvutru <62633356+Sondeptraivodichvutru@users.noreply.github.com>
2020-04-01 09:00:26 +0700

    c

--- demodbH/getConnect.php
+++ demodbH/getConnect.php
@@ -3,7 +3,7 @@
 $host = "ec2-52-207-93-32.compute-1.amazonaws.com";
 $username = "cfpqcmieuguxzc";
 $password = "be1ff296c1e77f1c98793a8f224816674566f363be7ef2ba394d98aa8e9bb64a";
-$database = "d425mt4bhmok93";
+$database = "d425mt4bhmok934";
 $port = "5432";
 $conn = mysqli_connect($host, $username, $port, $password, $database);
 mysqli_query($conn, $database);
