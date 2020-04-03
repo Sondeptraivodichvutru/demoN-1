153bee780e4db332a040ddabb62189655d23831b
Sondeptraivodichvutru <62633356+Sondeptraivodichvutru@users.noreply.github.com>
2020-04-03 08:21:11 +0700

    8h21

--- demodbH/Product/index.php
+++ demodbH/Product/index.php
@@ -590,35 +590,36 @@
                                     <!-- Product Panel -->
                                     <div class="product_panel panel active">
                                         <div class="featured_slider slider">
-                                            <?PHP
-                                            include("takep.php");
-                                                while ($row = mysql_fetch_row($query)) {
-                                                    ?>
-                                                    <div class="featured_slider_item">
-                                                        <div class="border_active"></div>
-                                                        <div class="product_item discount d-flex flex-column align-items-center justify-content-center text-center">
-                                                            <div class="product_image d-flex flex-column align-items-center justify-content-center"><img src="<?= $row[3] ?>" alt=""></div>
-                                                            <div class="product_content">
-                                                                <div class="product_price discount">$<?= $row[1] ?></div>
-                                                                <div class="product_name"><div><a href="product.html"><?= $row[0] ?></a></div></div>
-                                                        <div class="product_extras">
-                                                            <div class="product_color">
-                                                                <input type="radio" checked name="product_color" style="background:#b19c83">
-                                                                <input type="radio" name="product_color" style="background:#000000">
-                                                                <input type="radio" name="product_color" style="background:#999999">
+                                            <div class="featured_slider_item">
+                                                <div class="border_active"></div>
+                                                <div class="product_item discount d-flex flex-column align-items-center justify-content-center text-center">
+                                                    <?PHP
+                                                    include("takep.php");
+                                                    while ($row = mysql_fetch_row($query)) {
+                                                        ?>
+                                                        <div class="product_image d-flex flex-column align-items-center justify-content-center"><img src="<?= $row[3] ?>" alt=""></div>
+                                                        <div class="product_content">
+                                                            <div class="product_price discount">$<?= $row[1] ?></div>
+                                                            <div class="product_name"><div><a href="product.html"><?= $row[0] ?></a></div></div>
+                                                            <div class="product_extras">
+                                                                <div class="product_color">
+                                                                    <input type="radio" checked name="product_color" style="background:#b19c83">
+                                                                    <input type="radio" name="product_color" style="background:#000000">
+                                                                    <input type="radio" name="product_color" style="background:#999999">
+                                                                </div>
+                                                                <button class="product_cart_button">Add to Cart</button>
                                                             </div>
-                                                            <button class="product_cart_button">Add to Cart</button>
                                                         </div>
-                                                    </div>
-                                                    <div class="product_fav"><i class="fas fa-heart"></i></div>
-                                                    <ul class="product_marks">
-                                                        <li class="product_mark product_new">new</li>
-                                                    </ul>
+                                                        <div class="product_fav"><i class="fas fa-heart"></i></div>
+                                                        <ul class="product_marks">
+                                                            <li class="product_mark product_new">new</li>
+                                                        </ul>
+                                                        <?php
+                                                    }
+                                                    ?>
                                                 </div>
                                             </div>
-                                            <?php
-                                                }
-                                            ?>
+
                                             <!-- Slider Item -->
 
                                         </div>
